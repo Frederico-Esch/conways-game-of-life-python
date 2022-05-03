f91053048ef2401101b713ca9655808f1aa9312e
Frederico <fredeschpe@gmail.com>
2022-05-03 10:10:00 -0300

    Fix logic | working properly now

--- game_of_life.py
+++ game_of_life.py
@@ -5,19 +5,25 @@ class Node:
     size:int
     _x:int
     _y:int
-    colour: tuple[int,int,int]
+
+    colour: dict[int, tuple[int,int,int]]
+
     def __init__(self, _x, _y, size)->None:
         self.x = _x*size
         self.y = _y*size
         self.size = size
         self._x = _x
         self._y = _y
-        self.colour = (255,255,255)
-    def isAlive(self) -> bool:
-        return self.colour == ACTIVE_COLOUR
-    def born(self) -> None:
-        self.colour = ACTIVE_COLOUR
-    def die(self) -> None:
-        self.colour = (255,255,255)
+        self.colour = {
+            True: (255,255,255),
+            False: (255,255,255)
+        }
+
+    def isAlive(self, state:bool) -> bool:
+        return self.colour[state] == ACTIVE_COLOUR
+    def born(self, state:bool) -> None:
+        self.colour[state] = ACTIVE_COLOUR
+    def die(self, state:bool) -> None:
+        self.colour[state] = (255,255,255)
     def __iter__(self):
         return iter(self.x, self.y, self.colour)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ MOUSE_RELEASED = False
 SIZE = 600
 size = 10
 amount = SIZE//size
+state = True
 
 last_mouse = (-1, -1)
 neighbours_loc = (
@@ -24,10 +25,10 @@ pg.init()
 screen = pg.display.set_mode((SIZE, SIZE))
 grid = [[Node(i,j,size) for i in range(amount)] for j in range(amount)]
 
-def reset_grid():
+def reset_grid(estado:bool):
     for row in grid:
         for node in row:
-            node.die()
+            node.die(estado)
 
 def mouse_click() -> None:
     global MOUSE_RELEASED
@@ -40,17 +41,17 @@ def mouse_click() -> None:
         last_mouse = (-1, -1)
         return
     elif buttons[2] :
-        reset_grid()
+        reset_grid(state)
         return 
     x //= size
     y //= size
     if(last_mouse != (x,y)):
         #print(x,y)
         node = grid[x][y]
-        if not node.isAlive():
-            node.born()
+        if not node.isAlive(state):
+            node.born(state)
         else:
-            node.die()
+            node.die(state)
         last_mouse = (x,y)
 
 def pool_events() -> None:
@@ -69,15 +70,18 @@ def pool_events() -> None:
             if(pg.key.get_pressed()[pg.K_SPACE]):
                 iterate = not iterate
 
+clock = pg.time.Clock()
 while True:
     pool_events()
+    if iterate:
+        clock.tick(10)
 
     screen.fill((150,150,150))
 
     for rows in grid:
         for node in rows:
             #x, y, colour = rect
-            pg.draw.rect(screen, node.colour, (node.x, node.y, size-2, size-2))
+            pg.draw.rect(screen, node.colour[state], (node.x, node.y, size-2, size-2))
 
             if iterate:
                 _x = node._x
@@ -87,10 +91,17 @@ while True:
                     nx = _x + neigh[0]
                     ny = _y + neigh[1]
                     if nx >= 0 and nx < amount and ny >= 0 and ny < amount:
-                        if grid[ny][nx].isAlive():
+                        if grid[ny][nx].isAlive(state):
                             n_neighbours+=1
                 if n_neighbours == 3:
-                    grid[_y][_x].born()
+                    grid[_y][_x].born(not state)
                 elif n_neighbours < 2 or n_neighbours > 3:
-                    grid[_y][_x].die()
+                    grid[_y][_x].die(not state)
+                else:
+                    if grid[_y][_x].isAlive(state):
+                        grid[_y][_x].born(not state)
+                    else:
+                        grid[_y][_x].die(not state)
+    if iterate: state = not state
+    else: reset_grid(not state)
     pg.display.update()
